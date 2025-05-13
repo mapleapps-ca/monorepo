1afcefea3993a766b40f560e174af569c2f03825
Bartlomiej Mika <bart@mikasoftware.com>
2025-05-13 01:17:54 -0400

    Added user account info.

--- native/desktop/papercloud-cli/internal/config/config.go
+++ native/desktop/papercloud-cli/internal/config/config.go
@@ -1,16 +1,37 @@
 // monorepo/native/desktop/papercloud-cli/config/config.go
 package config
 
-import "context"
+import (
+	"context"
+	"time"
+
+	"github.com/mapleapps-ca/monorepo/native/desktop/papercloud-cli/internal/domain/keys"
+)
 
 // Config holds all application configuration
 type Config struct {
 	Application ApplicationConfig
+	Account     AccountConfig
 }
 
 // ApplicationConfig holds application-specific configuration
 type ApplicationConfig struct {
 	CloudProviderAddress string `json:"cloud_provider_address"`
+}
+
+type AccountConfig struct {
+	Email                             string                                 `json:"email" bson:"email"`
+	PasswordSalt                      []byte                                 `json:"password_salt" bson:"password_salt"`
+	EncryptedMasterKey                keys.EncryptedMasterKey                `json:"encrypted_master_key" bson:"encrypted_master_key"`
+	PublicKey                         keys.PublicKey                         `json:"public_key" bson:"public_key"`
+	EncryptedPrivateKey               keys.EncryptedPrivateKey               `json:"encrypted_private_key" bson:"encrypted_private_key"`
+	EncryptedRecoveryKey              keys.EncryptedRecoveryKey              `json:"encrypted_recovery_key" bson:"encrypted_recovery_key"`
+	MasterKeyEncryptedWithRecoveryKey keys.MasterKeyEncryptedWithRecoveryKey `json:"master_key_encrypted_with_recovery_key" bson:"master_key_encrypted_with_recovery_key"`
+	VerificationID                    string                                 `json:"verificationID"`
+	AccessToken                       string                                 `json:"access_token"`
+	AccessTokenExpiryTime             time.Time                              `json:"access_token_expiry_time"`
+	RefreshToken                      string                                 `json:"refresh_token"`
+	RefreshTokenExpiryTime            time.Time                              `json:"refresh_token_expiry_time"`
 }
 
 // ConfigRepository defines the interface for loading and saving configuration
