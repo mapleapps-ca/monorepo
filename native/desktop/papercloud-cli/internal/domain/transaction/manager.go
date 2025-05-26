b5733b631ddb0afb3352e68702e7a0c6503dab1a
Bartlomiej Mika <bart@mikasoftware.com>
2025-05-26 02:18:14 -0400

    🗑️ Removed deprecated code.

--- native/desktop/papercloud-cli/internal/domain/transaction/manager.go
+++
@@ -1,9 +0,0 @@
-// internal/domain/transaction/manager.go
-package transaction
-
-// Manager defines the interface for transaction management
-type Manager interface {
-	Begin() error
-	Commit() error
-	Rollback()
-}
