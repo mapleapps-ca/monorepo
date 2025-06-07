4aea311d90c99b53178d75c6e7b394481a53d42f
Bartlomiej Mika <bart@mikasoftware.com>
2025-06-06 23:37:41 -0400

    More progress for making our code cassandra-native.

--- cloud/mapleapps-backend/internal/iam/domain/federateduser/model.go
+++ cloud/mapleapps-backend/internal/iam/domain/federateduser/model.go
@@ -18,40 +18,35 @@ const (
 	FederatedUserRoleIndividual = 3 // Individual user, has permissions for individual-related operations
 )
 
-type FederatedUser struct {
-	ID                                             gocql.UUID `bson:"_id" json:"id"`
-	Email                                          string     `bson:"email" json:"email"`
-	FirstName                                      string     `bson:"first_name" json:"first_name"`
-	LastName                                       string     `bson:"last_name" json:"last_name"`
-	Name                                           string     `bson:"name" json:"name"`
-	LexicalName                                    string     `bson:"lexical_name" json:"lexical_name"`
-	Role                                           int8       `bson:"role" json:"role"`
-	Status                                         int8       `bson:"status" json:"status"`
-	WasEmailVerified                               bool       `bson:"was_email_verified" json:"was_email_verified,omitempty"`
-	EmailVerificationCode                          string     `bson:"email_verification_code,omitempty" json:"email_verification_code,omitempty"`
-	EmailVerificationExpiry                        time.Time  `bson:"email_verification_expiry,omitempty" json:"email_verification_expiry"`
-	PasswordResetVerificationCode                  string     `bson:"password_reset_verification_code,omitempty" json:"password_reset_verification_code,omitempty"`
-	PasswordResetVerificationExpiry                time.Time  `bson:"password_reset_verification_expiry,omitempty" json:"password_reset_verification_expiry"`
-	Phone                                          string     `bson:"phone" json:"phone,omitempty"`
-	Country                                        string     `bson:"country" json:"country,omitempty"`
-	Timezone                                       string     `bson:"timezone" json:"timezone"`
-	Region                                         string     `bson:"region" json:"region,omitempty"`
-	City                                           string     `bson:"city" json:"city,omitempty"`
-	PostalCode                                     string     `bson:"postal_code" json:"postal_code,omitempty"`
-	AddressLine1                                   string     `bson:"address_line1" json:"address_line1,omitempty"`
-	AddressLine2                                   string     `bson:"address_line2" json:"address_line2,omitempty"`
-	HasShippingAddress                             bool       `bson:"has_shipping_address" json:"has_shipping_address,omitempty"`
-	ShippingName                                   string     `bson:"shipping_name" json:"shipping_name,omitempty"`
-	ShippingPhone                                  string     `bson:"shipping_phone" json:"shipping_phone,omitempty"`
-	ShippingCountry                                string     `bson:"shipping_country" json:"shipping_country,omitempty"`
-	ShippingRegion                                 string     `bson:"shipping_region" json:"shipping_region,omitempty"`
-	ShippingCity                                   string     `bson:"shipping_city" json:"shipping_city,omitempty"`
-	ShippingPostalCode                             string     `bson:"shipping_postal_code" json:"shipping_postal_code,omitempty"`
-	ShippingAddressLine1                           string     `bson:"shipping_address_line1" json:"shipping_address_line1,omitempty"`
-	ShippingAddressLine2                           string     `bson:"shipping_address_line2" json:"shipping_address_line2,omitempty"`
-	AgreeTermsOfService                            bool       `bson:"agree_terms_of_service" json:"agree_terms_of_service,omitempty"`
-	AgreePromotions                                bool       `bson:"agree_promotions" json:"agree_promotions,omitempty"`
-	AgreeToTrackingAcrossThirdPartyAppsAndServices bool       `bson:"agree_to_tracking_across_third_party_apps_and_services" json:"agree_to_tracking_across_third_party_apps_and_services,omitempty"`
+type FederatedUserProfileData struct {
+	Phone                                          string `bson:"phone" json:"phone,omitempty"`
+	Country                                        string `bson:"country" json:"country,omitempty"`
+	Timezone                                       string `bson:"timezone" json:"timezone"`
+	Region                                         string `bson:"region" json:"region,omitempty"`
+	City                                           string `bson:"city" json:"city,omitempty"`
+	PostalCode                                     string `bson:"postal_code" json:"postal_code,omitempty"`
+	AddressLine1                                   string `bson:"address_line1" json:"address_line1,omitempty"`
+	AddressLine2                                   string `bson:"address_line2" json:"address_line2,omitempty"`
+	HasShippingAddress                             bool   `bson:"has_shipping_address" json:"has_shipping_address,omitempty"`
+	ShippingName                                   string `bson:"shipping_name" json:"shipping_name,omitempty"`
+	ShippingPhone                                  string `bson:"shipping_phone" json:"shipping_phone,omitempty"`
+	ShippingCountry                                string `bson:"shipping_country" json:"shipping_country,omitempty"`
+	ShippingRegion                                 string `bson:"shipping_region" json:"shipping_region,omitempty"`
+	ShippingCity                                   string `bson:"shipping_city" json:"shipping_city,omitempty"`
+	ShippingPostalCode                             string `bson:"shipping_postal_code" json:"shipping_postal_code,omitempty"`
+	ShippingAddressLine1                           string `bson:"shipping_address_line1" json:"shipping_address_line1,omitempty"`
+	ShippingAddressLine2                           string `bson:"shipping_address_line2" json:"shipping_address_line2,omitempty"`
+	AgreeTermsOfService                            bool   `bson:"agree_terms_of_service" json:"agree_terms_of_service,omitempty"`
+	AgreePromotions                                bool   `bson:"agree_promotions" json:"agree_promotions,omitempty"`
+	AgreeToTrackingAcrossThirdPartyAppsAndServices bool   `bson:"agree_to_tracking_across_third_party_apps_and_services" json:"agree_to_tracking_across_third_party_apps_and_services,omitempty"`
+}
+
+type FederatedUserSecurityData struct {
+	WasEmailVerified bool `bson:"was_email_verified" json:"was_email_verified,omitempty"`
+
+	Code       string    `bson:"code,omitempty" json:"code,omitempty"`
+	CodeType   string    `bson:"code_type,omitempty" json:"code_type,omitempty"` // -- 'email_verification' or 'password_reset'
+	CodeExpiry time.Time `bson:"code_expiry,omitempty" json:"code_expiry"`
 
 	// --- E2EE Related ---
 	PasswordSalt []byte `json:"password_salt" bson:"password_salt"`
@@ -74,17 +69,6 @@ type FederatedUser struct {
 	LastKeyRotation   *time.Time              `json:"last_key_rotation,omitempty" bson:"last_key_rotation,omitempty"`
 	KeyRotationPolicy *keys.KeyRotationPolicy `json:"key_rotation_policy,omitempty" bson:"key_rotation_policy,omitempty"`
 
-	// --- Metadata ---
-	CreatedFromIPAddress  string     `bson:"created_from_ip_address" json:"created_from_ip_address"`
-	CreatedByUserID       gocql.UUID `bson:"created_by_user_id" json:"created_by_user_id"`
-	CreatedAt             time.Time  `bson:"created_at" json:"created_at"`
-	CreatedByName         string     `bson:"created_by_name" json:"created_by_name"`
-	ModifiedFromIPAddress string     `bson:"modified_from_ip_address" json:"modified_from_ip_address"`
-	ModifiedByUserID      gocql.UUID `bson:"modified_by_user_id" json:"modified_by_user_id"`
-	ModifiedAt            time.Time  `bson:"modified_at" json:"modified_at"`
-	ModifiedByName        string     `bson:"modified_by_name" json:"modified_by_name"`
-	LastLoginAt           time.Time  `json:"last_login_at" bson:"last_login_at"`
-
 	// OTPEnabled controls whether we force 2FA or not during login.
 	OTPEnabled bool `bson:"otp_enabled" json:"otp_enabled"`
 
@@ -106,6 +90,34 @@ type FederatedUser struct {
 
 	// OTPBackupCodeHashAlgorithm tracks the hashing algorithm used.
 	OTPBackupCodeHashAlgorithm string `bson:"otp_backup_code_hash_algorithm" json:"-"`
+}
+
+type FederatedUserMetadata struct {
+	CreatedFromIPAddress  string     `bson:"created_from_ip_address" json:"created_from_ip_address"`
+	CreatedByUserID       gocql.UUID `bson:"created_by_user_id" json:"created_by_user_id"`
+	CreatedAt             time.Time  `bson:"created_at" json:"created_at"`
+	CreatedByName         string     `bson:"created_by_name" json:"created_by_name"`
+	ModifiedFromIPAddress string     `bson:"modified_from_ip_address" json:"modified_from_ip_address"`
+	ModifiedByUserID      gocql.UUID `bson:"modified_by_user_id" json:"modified_by_user_id"`
+	ModifiedAt            time.Time  `bson:"modified_at" json:"modified_at"`
+	ModifiedByName        string     `bson:"modified_by_name" json:"modified_by_name"`
+	LastLoginAt           time.Time  `json:"last_login_at" bson:"last_login_at"`
+}
+
+type FederatedUser struct {
+	ID           gocql.UUID                 `bson:"_id" json:"id"`
+	Email        string                     `bson:"email" json:"email"`
+	FirstName    string                     `bson:"first_name" json:"first_name"`
+	LastName     string                     `bson:"last_name" json:"last_name"`
+	Name         string                     `bson:"name" json:"name"`
+	LexicalName  string                     `bson:"lexical_name" json:"lexical_name"`
+	Role         int8                       `bson:"role" json:"role"`
+	Status       int8                       `bson:"status" json:"status"`
+	CreatedAt    time.Time                  `bson:"created_at" json:"created_at"`
+	ModifiedAt   time.Time                  `bson:"modified_at" json:"modified_at"`
+	ProfileData  *FederatedUserProfileData  `bson:"profile_data" json:"profile_data"`
+	SecurityData *FederatedUserSecurityData `bson:"security_data" json:"security_data"`
+	Metadata     *FederatedUserMetadata     `bson:"metadata" json:"metadata"`
 }
 
 // FederatedUserFilter represents the filter criteria for listing users
