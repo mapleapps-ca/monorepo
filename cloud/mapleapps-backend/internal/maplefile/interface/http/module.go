4562d2bc1d9d64d247a0f026a6e22f79ecbc6027
Bartlomiej Mika <bart@mikasoftware.com>
2025-06-09 19:53:07 -0400

    Enabled more http interfaces.

--- cloud/mapleapps-backend/internal/maplefile/interface/http/module.go
+++ cloud/mapleapps-backend/internal/maplefile/interface/http/module.go
@@ -5,11 +5,12 @@ import (
 	"go.uber.org/fx"
 
 	unifiedhttp "github.com/mapleapps-ca/monorepo/cloud/mapleapps-backend/internal/manifold/interface/http"
-	// "github.com/mapleapps-ca/monorepo/cloud/mapleapps-backend/internal/maplefile/interface/http/collection"
+	"github.com/mapleapps-ca/monorepo/cloud/mapleapps-backend/internal/maplefile/interface/http/collection"
 	commonhttp "github.com/mapleapps-ca/monorepo/cloud/mapleapps-backend/internal/maplefile/interface/http/common"
+	"github.com/mapleapps-ca/monorepo/cloud/mapleapps-backend/internal/maplefile/interface/http/me"
+	"github.com/mapleapps-ca/monorepo/cloud/mapleapps-backend/internal/maplefile/interface/http/middleware"
 	// "github.com/mapleapps-ca/monorepo/cloud/mapleapps-backend/internal/maplefile/interface/http/file"
 	// "github.com/mapleapps-ca/monorepo/cloud/mapleapps-backend/internal/maplefile/interface/http/me"
-	"github.com/mapleapps-ca/monorepo/cloud/mapleapps-backend/internal/maplefile/interface/http/middleware"
 )
 
 func Module() fx.Option {
@@ -21,36 +22,35 @@ func Module() fx.Option {
 			// Common handlers
 			unifiedhttp.AsRoute(commonhttp.NewMapleFileVersionHTTPHandler),
 
-			// // Me handlers
-			// unifiedhttp.AsRoute(me.NewGetMeHTTPHandler),
-			// unifiedhttp.AsRoute(me.NewPutUpdateMeHTTPHandler),
-			// unifiedhttp.AsRoute(me.NewDeleteMeHTTPHandler),
+			// Me handlers
+			unifiedhttp.AsRoute(me.NewGetMeHTTPHandler),
+			unifiedhttp.AsRoute(me.NewPutUpdateMeHTTPHandler),
+			unifiedhttp.AsRoute(me.NewDeleteMeHTTPHandler),
 
-			// // // Collection handlers - Basic CRUD
-			// // unifiedhttp.AsRoute(collection.NewCreateCollectionHTTPHandler),
-			// // unifiedhttp.AsRoute(collection.NewGetCollectionHTTPHandler),
-			// // unifiedhttp.AsRoute(collection.NewListUserCollectionsHTTPHandler),
-			// // unifiedhttp.AsRoute(collection.NewUpdateCollectionHTTPHandler),
-			// // unifiedhttp.AsRoute(collection.NewSoftDeleteCollectionHTTPHandler),
-			// // unifiedhttp.AsRoute(collection.NewArchiveCollectionHTTPHandler),
-			// // unifiedhttp.AsRoute(collection.NewRestoreCollectionHTTPHandler),
+			// Collection handlers - Basic CRUD
+			unifiedhttp.AsRoute(collection.NewCreateCollectionHTTPHandler),
+			unifiedhttp.AsRoute(collection.NewGetCollectionHTTPHandler),
+			unifiedhttp.AsRoute(collection.NewListUserCollectionsHTTPHandler),
+			unifiedhttp.AsRoute(collection.NewUpdateCollectionHTTPHandler),
+			unifiedhttp.AsRoute(collection.NewSoftDeleteCollectionHTTPHandler),
+			unifiedhttp.AsRoute(collection.NewArchiveCollectionHTTPHandler),
+			unifiedhttp.AsRoute(collection.NewRestoreCollectionHTTPHandler),
 
-			// // // Collection handlers - Hierarchical operations
-			// // unifiedhttp.AsRoute(collection.NewFindCollectionsByParentHTTPHandler),
-			// // unifiedhttp.AsRoute(collection.NewFindRootCollectionsHTTPHandler),
-			// // unifiedhttp.AsRoute(collection.NewGetCollectionHierarchyHTTPHandler),
-			// // unifiedhttp.AsRoute(collection.NewMoveCollectionHTTPHandler),
+			// Collection handlers - Hierarchical operations
+			unifiedhttp.AsRoute(collection.NewFindCollectionsByParentHTTPHandler),
+			unifiedhttp.AsRoute(collection.NewFindRootCollectionsHTTPHandler),
+			unifiedhttp.AsRoute(collection.NewMoveCollectionHTTPHandler),
 
-			// // // Collection handlers - Sharing
-			// // unifiedhttp.AsRoute(collection.NewShareCollectionHTTPHandler),
-			// // unifiedhttp.AsRoute(collection.NewRemoveMemberHTTPHandler),
-			// // unifiedhttp.AsRoute(collection.NewListSharedCollectionsHTTPHandler),
+			// Collection handlers - Sharing
+			unifiedhttp.AsRoute(collection.NewShareCollectionHTTPHandler),
+			unifiedhttp.AsRoute(collection.NewRemoveMemberHTTPHandler),
+			unifiedhttp.AsRoute(collection.NewListSharedCollectionsHTTPHandler),
 
-			// // // Collection handlers - Filtered operations
-			// // unifiedhttp.AsRoute(collection.NewGetFilteredCollectionsHTTPHandler),
+			// Collection handlers - Filtered operations
+			unifiedhttp.AsRoute(collection.NewGetFilteredCollectionsHTTPHandler),
 
-			// // // Sync handlers
-			// // unifiedhttp.AsRoute(collection.NewCollectionSyncHTTPHandler),
+			// Sync handlers
+			unifiedhttp.AsRoute(collection.NewCollectionSyncHTTPHandler),
 
 			// // // File handlers
 			// // unifiedhttp.AsRoute(file.NewSoftDeleteFileHTTPHandler),
